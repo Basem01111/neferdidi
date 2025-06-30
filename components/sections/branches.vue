57b5cb3bee33c6e05452793d576f0042af1e0dd1
Basem01111 <basemgaber01111@gmail.com>
2025-06-30 15:17:28 +0300

    Edit Modal Home Page

--- components/sections/branches.vue
+++ components/sections/branches.vue
@@ -24,6 +24,7 @@
       :btn="{
         text: '申请加盟',
         link: '/',
+        notLink: true
       }"
     />
 
--- components/ui/boxData.vue
+++ components/ui/boxData.vue
@@ -19,13 +19,27 @@
       </p>
 
       <!-- Data -->
-      <div class="order-2 md:order-3 flex m-[5.3vw_0] md:m-0 md:mt-[34px] justify-center">
-        <div v-for="(item, i) in data" :key="i" class="border-e border-[var(--main-color-border-3)] last:border-none text-center flex-[1_1]">
+      <div
+        class="order-2 md:order-3 flex m-[5.3vw_0] md:m-0 md:mt-[34px] justify-center"
+      >
+        <div
+          v-for="(item, i) in data"
+          :key="i"
+          class="border-e border-[var(--main-color-border-3)] last:border-none text-center flex-[1_1]"
+        >
           <!-- Title Data -->
-          <h5 class="mb-0 text-center text-[4.27vw] md:text-base md:tracking-[unset] tracking-[0.05em] font-semibold text-[var(--main-color)]">{{ item.title }}</h5>
+          <h5
+            class="mb-0 text-center text-[4.27vw] md:text-base md:tracking-[unset] tracking-[0.05em] font-semibold text-[var(--main-color)]"
+          >
+            {{ item.title }}
+          </h5>
 
           <!-- Items Data -->
-          <p v-for="(text, j) in item.text" :key="j" class="mb-0 first:text-[3.47vw] text-[2.6vw] md:text-sm font-normal text-[var(--main-color-text-2)] leading-[1.5]">
+          <p
+            v-for="(text, j) in item.text"
+            :key="j"
+            class="mb-0 first:text-[3.47vw] text-[2.6vw] md:text-sm font-normal text-[var(--main-color-text-2)] leading-[1.5]"
+          >
             {{ text }}
           </p>
         </div>
@@ -39,19 +53,23 @@
         <NuxtImg
           :src="img"
           class="w-[89px] md:w-[160px] h-auto md:mt-[60px] m-[18.4vw_auto_5.6vw] md:m-0"
-          :class="{'!w-[15.09vw] md:!w-[107px] !m-[18.4vw_auto_5.6vw] md:!m-0': styleBox == '2'}"
+          :class="{
+            '!w-[15.09vw] md:!w-[107px] !m-[18.4vw_auto_5.6vw] md:!m-0':
+              styleBox == '2',
+          }"
         />
       </div>
 
       <!-- Btn -->
       <div
         class="hidden md:flex gap-[15px] items-center w-auto cursor-pointer mt-[40px]"
-        :class="{'!mt-[50px]': styleBox == '2'}"
+        :class="{ '!mt-[50px]': styleBox == '2' }"
       >
         <span class="text-[var(--main-color-text)] text-base">
           {{ btn?.text }}
         </span>
         <NuxtLink
+          v-if="!btn?.notLink"
           :to="btn?.link"
           type="button"
           class="bg-transparent border-0 outline-0 cursor-pointer transition-all duration-400 ease-in-out hover:translate-x-[10px]"
@@ -61,6 +79,26 @@
             class="h-[33px] w-auto"
           />
         </NuxtLink>
+
+        <button
+          v-else
+          :to="btn?.link"
+          type="button"
+          class="bg-transparent border-0 outline-0 cursor-pointer transition-all duration-400 ease-in-out hover:translate-x-[10px]"
+          @click="showModal = !showModal"
+        >
+          <NuxtImg
+            src="/images/global/arrow-right.png"
+            class="h-[33px] w-auto"
+          />
+        </button>
+
+        <!-- Modal -->
+        <Teleport to="body">
+          <UiModal :show-modal="showModal" @close="showModal = false">
+            <UiBoxQrLink />
+          </UiModal>
+        </Teleport>
       </div>
     </div>
   </div>
@@ -93,6 +131,7 @@ defineProps({
     default: () => {},
   },
 });
+const showModal = ref(false);
 </script>
 
 <style scoped lang="scss"></style>
